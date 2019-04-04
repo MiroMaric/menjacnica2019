5a0af70ab10a45df4e15259697c0f538fbcfb304
MiroMaric <miromaric1996@gmail.com>
2019-04-05 01:11:10 +0200

    Dodata implementacija metode dodajKurs

--- Menjacnica2019/src/menjacnica/Menjacnica.java
+++ Menjacnica2019/src/menjacnica/Menjacnica.java
@@ -1,15 +1,33 @@
 package menjacnica;
 
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 import domen.Kurs;
+import domen.Valuta;
 
 public class Menjacnica implements IMenjacnica{
 
+private List<Valuta> valute;
+	
+	public Menjacnica() {
+		valute = new LinkedList<>();
+	}
+
 	@Override
 	public void dodajKurs(String valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
-		
+		if(valuta==null || kurs==null)
+			throw new RuntimeException("Podaci o valuti i kursu moraju biti validni");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		for(Valuta v:valute){
+			if(v.equals(valuta)){
+				v.getDnevniKursevi().put(sdf.format(kurs.getDatum().getTime()), kurs);
+				return;
+			}
+		}
+		throw new RuntimeException("Ne postoji prosledjena valuta");
 	}
 
 	@Override
