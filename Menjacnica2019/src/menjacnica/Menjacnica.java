d49cc43a216bd9399c4b4a03ac71f24b7de6d9e1
MiroMaric <miromaric1996@gmail.com>
2019-04-05 00:40:41 +0200

    Napravljena klasa Menjacnica koja implementira IMenjacnica

---
+++ Menjacnica2019/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import domen.Kurs;
+
+public class Menjacnica implements IMenjacnica{
+
+	@Override
+	public void dodajKurs(String valuta, Kurs kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
