dd3a70d45dc21b1c4f0958ae831b23e5f9d8d298
MiroMaric <miromaric1996@gmail.com>
2019-04-05 00:13:15 +0200

    Dodate toString,hashCode i equals metode u Kurs i Valuta klase

--- Menjacnica2019/src/domen/Kurs.java
+++ Menjacnica2019/src/domen/Kurs.java
@@ -1,5 +1,6 @@
 package domen;
 
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 public class Kurs {
@@ -51,6 +52,50 @@ public class Kurs {
 
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + kupovni;
+		result = prime * result + prodajni;
+		result = prime * result + srednji;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!sdf.format(datum.getTime()).equals(sdf.format(other.datum.getTime())))
+			return false;
+		if (kupovni != other.kupovni)
+			return false;
+		if (prodajni != other.prodajni)
+			return false;
+		if (srednji != other.srednji)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Kurs [prodajni=" + prodajni + ", srednji=" + srednji + ", kupovni=" + kupovni + ", datum=" + 
+				new SimpleDateFormat("dd.MM.yyyy").format(datum.getTime())
+				+ "]";
 	}
 	
 }
--- Menjacnica2019/src/domen/Valuta.java
+++ Menjacnica2019/src/domen/Valuta.java
@@ -48,5 +48,44 @@ public class Valuta {
 	public void setDnevniKursevi(HashMap<String, Kurs> dnevniKursevi) {
 		this.dnevniKursevi = dnevniKursevi;
 	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skracenNaziv == null) ? 0 : skracenNaziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skracenNaziv == null) {
+			if (other.skracenNaziv != null)
+				return false;
+		} else if (!skracenNaziv.equals(other.skracenNaziv))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skracenNaziv=" + skracenNaziv + "]";
+	}
+	
 	
 }
